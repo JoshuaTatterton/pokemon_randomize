349bb572f397d16272ffaad0e930f7867707d38d
Josh Tatterton <joshua.tatterton@matalandirect.com>
2016-04-24 15:26:49 +0100

    Reconfigure code to use static images instead

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root to: "options#index"
 
   get "options" => "options#index"
-  get "randomize" => "randomize#index"
+  post "randomize" => "randomize#index"
   
 end
